6041e0a72a1329474fc48e549562eafb63cea524
Vikas Mehra <vickeyvikasmehra@gmail.com>
2025-07-14 01:35:43 +0530

    feat: add question format in the prompt

--- quiz-generator-ai-agent-api/agent/agent.go
+++ quiz-generator-ai-agent-api/agent/agent.go
@@ -6,9 +6,39 @@ import (
 )
 
 func AgentHandler(user_request models.UserRequest) (any, error) {
+
+	question_model := ` Each question must be a JSON object with these exact fields:{
+		"QuestionId": [integer],
+		"Ques": "[clear, specific question]",
+		"OptionA": "[first option]",
+		"OptionB": "[second option]",
+		"OptionC": "[third option]",
+		"OptionD": "[fourth option]",
+		"Answer": "[complete correct answer text matching one of the options exactly]"
+	}
+	for example,
+	[{
+		"QuestionId": 1,
+		"Ques": "What is a qubit?",
+		"OptionA": "A bit that can be either 0 or 1.",
+		"OptionB": "A bit that can exist in multiple states simultaneously.",
+		"OptionC": "A physical wire used to transmit quantum data.",
+		"OptionD": "A type of classical computer.",
+		"Answer": "A bit that can exist in multiple states simultaneously."
+	},
+	{
+        "QuestionId": 2,
+        "Ques": "Where is the Indus Valley Civilization located?",
+        "OptionA": "In ancient Egypt",
+        "OptionB": "In ancient Mesopotamia",
+        "OptionC": "In modern-day Pakistan and India",
+        "OptionD": "In South America",
+        "Answer": "In modern-day Pakistan and India"
+	},.... ]`
+
 	var user_message models.Message
 	user_message.Role = "user"
-	user_message.Content = "Generate " + user_request.NoQ + " questions about this topic: " + user_request.Topic + " with " + user_request.Difficulty + " difficulty."
+	user_message.Content = "Generate " + user_request.NoQ + " questions about this topic: " + user_request.Topic + " with " + user_request.Difficulty + " difficulty. " + question_model
 	messages := []models.Message{user_message}
 
 	result, err := LLMcall(messages)
